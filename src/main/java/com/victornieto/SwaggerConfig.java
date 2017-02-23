7f27b2420eeac61a0068956e07deca3e52edae11
Victor Nieto <victor.nieto.castan@accenture.com>
2017-02-23 19:27:16 +0100

    Changing swagger config

--- src/main/java/com/victornieto/SwaggerConfig.java
+++ src/main/java/com/victornieto/SwaggerConfig.java
@@ -2,30 +2,39 @@ package com.victornieto;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("org.baeldung.web.controller")).paths(PathSelectors.ant("/foos/*")).build().apiInfo(apiInfo()).useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build(), new ResponseMessageBuilder().code(403).message("Forbidden!!!!!").build()));
+	@Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .select()
+            .paths(Predicates.not(PathSelectors.regex("/error.*")))
+            .build();
     }
 
-    private ApiInfo apiInfo() {
-        ApiInfo apiInfo = new ApiInfo("ApiRest Demo", "Some custom description of API.", "API TOS", "Terms of service", "myeaddress@company.com", "License of API", "API license URL");
-        return apiInfo;
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Demo Spring Boot API")
+            .description("API documentation for Spring Boot demo")
+            .contact(new Contact("name", "url", "email"))
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+            .version("2.0")
+            .build();
     }
 }
