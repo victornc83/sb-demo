217224f84e8d6b55cc7ef61ae415d4295afee4b5
Victor Nieto <victornc83@gmail.com>
2017-02-23 08:18:48 +0100

    Added Lombok library

--- src/main/java/com/victornieto/SbDemoApplication.java
+++ src/main/java/com/victornieto/SbDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SbDemoApplication.class, args);
+
 	}
 }
